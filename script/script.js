654062d67fbdba61e336a842693898b02bea75d5
Githubber2662 <95174200+Githubber2662@users.noreply.github.com>
2026-02-22 17:39:39 +0200

    Updated displayord function

--- script/script.js
+++ script/script.js
@@ -1741,7 +1741,7 @@ function displayOrd(ord,base=3,over=0,trim=0,large=0,multoff=0,colour=0) {
     if (ordColor == "no") ordColor="red"
     dispString += (colour==1?"<span style='color:red;text-shadow: 6px 6px 6px red, 1px 0 1px black, -1px 0 1px black, 0 1px 1px black, 0 -1px 1px black;'>" + Math.ceil(EN.add(ord,over).toNumber()) + "</span>":Math.ceil(EN.add(ord,over).toNumber()))
   }
-
+	dispString = dispString.replace("+0", "")
   return dispString;
 }
 
